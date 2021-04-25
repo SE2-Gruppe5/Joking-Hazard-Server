12507244632494eb231594b14aee6f789b59f387
Florian Dueller <florian.dueller@trinitec.at>
2021-04-26 00:46:40 +0200

    Send logs to file

--- src/utils/Logger.ts
+++ src/utils/Logger.ts
@@ -1,6 +1,9 @@
 import * as pino from "pino";
 
-export const logger = pino({
-  name: "joking-hazard-server",
-  level: "debug",
-});
+export const logger = pino(
+  {
+    name: "joking-hazard-server",
+    level: "debug",
+  },
+  pino.destination("./jh.log")
+);
