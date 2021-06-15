1d1b75a5033d563b9886f35a7fafc3e4e90f134a
Felix <felixgeb@edu.aau.at>
2021-06-15 17:25:12 +0200

    setTimeout verschoben

--- src/handlers/roomHandlers.ts
+++ src/handlers/roomHandlers.ts
@@ -362,10 +362,11 @@ export function storyConfirmed(io: Server, socket: Socket, userId: string,callba
   let currentPlayerId = game.players[game.currentPlayer];
   getSocketById(io, userId).then((socket) => {
     io.to(roomCode).emit("room:winner", { player: socket.data.name });
+    setTimeout(() => {
+      io.to(currentPlayerId).emit("room:your_turn", { judge: true });
+    }, 4000)
   });
-  setTimeout(() => {
-    io.to(currentPlayerId).emit("room:your_turn", { judge: true });
-  }, 4000)
+
 
 
 }
